72c46c3fd24d622fd78d874d2b4d488e3d3b746c
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-02-23 16:43:29 +0530

    client loader created

---
+++ components/Loader.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- global css added -->
+<link rel="stylesheet" href="./styles/style.css?v=2">
+<body>
+
+
+
+    <div class="loader_Container">
+
+        <div class="loader">
+            <img src="./images/loader.gif" alt="loader" class="image" />
+        </div>
+
+    </div>
+
+</body>
+
+</html>
