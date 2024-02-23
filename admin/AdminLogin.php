74dc0e337d1d840c609c4f902116aba89d1aa4b1
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-02-23 16:26:34 +0530

    admin fixing bug

--- admin/AdminLogin.php
+++ admin/AdminLogin.php
@@ -11,6 +11,10 @@
     if (window.history.replaceState) {
         window.history.replaceState(null, null, window.location.href)
     }
+
+  
+    window.document.title = "SD CANTEEN | Admin Login";
+
 </script>
 
 <body>
