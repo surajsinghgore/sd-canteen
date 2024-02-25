20a1adacb7cd96008acd4ca9fdabe21d20d36d06
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-02-25 20:21:21 +0530

    add food and drink items style fixed

--- admin/addcoffeeitem.php
+++ admin/addcoffeeitem.php
@@ -9,7 +9,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php require('../modules/HeadTag.php'); ?>
 
-<link rel="stylesheet" href="../styles/admin/admin.css?v=5">
+<link rel="stylesheet" href="../styles/admin/admin.css?v=18">
 <script>
     window.document.title = "SD CANTEEN | Add Coffee Item";
     // prevent reload post request
@@ -27,9 +27,11 @@
         <!-- left side bar import -->
         <?php require('../components/LeftAdminHeader.php'); ?>
 
-        <!-- right top header -->
+
+
         <div class="rightsidebar">
-            <?php $AdminTopHeaderTitle = "Add Coffee Page";
+     
+        <?php $AdminTopHeaderTitle = "Add Coffee Page";
             require('../components/AdminTopHeader.php'); ?>
 
 
@@ -40,125 +42,174 @@
             $pathNavigationChild = "add coffee item";
             require('../components/PathNavigation.php'); ?>
 
+   
 
-
-                <div class="Form">
-                    <form action="" method="post" enctype="multipart/form-data">
-                        <div class="heading">
-                            <h1>
-                                Enter New CoffeeItem Item For Website
-                            </h1>
-                        </div>
-                        <div class="form_element">
-                            <li>
-                                <p>
-                                    Enter Coffee Name <span>*</span>
-                                </p>
-                                <input type="text" name="CoffeeName" value="<?php if (isset($CoffeeName)) {
+        <div class="Form">
+          <div class="heading">
+            <h1>
+              Enter New Coffee   Item For Website
+            </h1>
+          </div>
+          <form method="post" action=""  enctype="multipart/form-data">
+          <div class="form_element">
+            <li>
+              <p>
+                Enter
+                Coffee
+                Name <span>*</span>
+              </p>
+              <input type="text" name="CoffeeName" value="<?php if (isset($CoffeeName)) {
                                                                                 echo $CoffeeName;
                                                                             } ?>"
                                                                             autofocus required>
-                            </li>
+                
+              
+            </li>
 
-                            <li>
-                                <p>
-                                    Enter Coffee Qty
-                                </p>
-                                <input type="Number" name="CoffeeQty" value="1" required>
-                            </li>
+            <li>
+              <p>
+                Enter Coffee
+                Qty
+              </p>
+              <input
+                type="text"
+                name="CoffeeQty"
+               value="1"
+               readonly
+          
+              />
+            </li>
 
-                            <li class="selects">
-                                <p>
-                                    Enter Coffee Category <span>*</span>
-                                </p>
-                                <select name="CoffeeCategory">
+            <li class="selects">
+              <p>
+                Enter
+                Coffee
+                Category <span>*</span>
+              </p>
+              <select
+                name="CoffeeCategory"
+             
+                
+              >
+                <option value="no">Select Category</option>
+                
+                <?php
 
+require('../middleware/ConnectToDatabase.php');
+$sql_query = "select * from coffeecategories";
+$resFoodItem = mysqli_query($connection, $sql_query);
 
-                                    <option value="no">Select coffee category</option>
+$length = mysqli_num_rows($resFoodItem);
 
+if ($length == 0) {
+    echo "<h6>No Item Found</h6>";
+} else {
+    while ($dataSearch = mysqli_fetch_array($resFoodItem)) {
+        echo "<option>";
+        echo $dataSearch['coffeecategoryname'];
+        echo "</option>";
+    }
+}
+?>
+                     
+              </select>
+            </li>
 
-                                    <!-- load category name -->
-                                    <?php
+       
 
-                                    require('../middleware/ConnectToDatabase.php');
-                                    $sql_query = "select * from coffeecategories";
-                                    $resFoodItem = mysqli_query($connection, $sql_query);
-
-                                    $length = mysqli_num_rows($resFoodItem);
-
-                                    if ($length == 0) {
-                                        echo "<h6>No Item Found</h6>";
-                                    } else {
-                                        while ($dataSearch = mysqli_fetch_array($resFoodItem)) {
-                                            echo "<option>";
-                                            echo $dataSearch['coffeecategoryname'];
-                                            echo "</option>";
-                                        }
-                                    }
-                                    ?>
-
-                                </select>
-                            </li>
-
-
-                            <li class="Prices">
-                                <h6>
-                                    Enter Price <span>*</span>
-                                </h6>
-                                <p>
-                                    <input type="text" name="normalPriceName" class="priceHeading" value="Normal Size Price" readonly>
-                                    <input type="Number" name="normalPrice" class="prices" value="<?php if (isset($normalPrice)) {
+              <li class="Pricess">
+                <h6>
+                  Enter Price <span>*</span>
+                </h6>
+                <p>
+                  <input
+                    type="text"
+                    name="normalPriceName"
+                    class="priceHeading"
+                    value="Normal Size Price" readonly
+                  >
+                  <input type="Number" name="normalPrice" class="prices" value="<?php if (isset($normalPrice)) {
                                                                                                         echo $normalPrice;
                                                                                                     } ?>">
-                                </p>
-                                <h4>Or</h4>
-                                <p>
-                                    <input type="text" name="smallPriceName" class="priceHeading" value="Small Size Price" readonly>
-                                    <input type="Number" name="smallPrice" class="prices" value="<?php if (isset($smallPrice)) {
+                </p>
+                <h4>Or</h4>
+                <p>
+                  <input
+                    type="text"
+                    name="smallPriceName"
+                    class="priceHeading"
+                    value="Small Size Price" 
+                    readonly
+                  >
+                  <input type="Number" name="smallPrice" class="prices" value="<?php if (isset($smallPrice)) {
                                                                                                         echo $smallPrice;
                                                                                                     } ?>">
-                                </p>
+                </p>
 
-                                <p>
-                                    <input type="text" name="mediumPriceName" class="priceHeading" value="Medium Size Price" readonly>
-                                    <input type="Number" name="mediumPrice" class="prices" value="<?php if (isset($mediumPrice)) {
+                <p>
+                  <input
+                    type="text"
+                    name="mediumPriceName"
+                    class="priceHeading"
+                
+                    value="Medium Size Price"
+                    readonly
+                  />
+                  <input type="Number" name="mediumPrice" class="prices" value="<?php if (isset($mediumPrice)) {
                                                                                                         echo $mediumPrice;
                                                                                                     } ?>">
-                                </p>
+                </p>
 
-                                <p>
-                                    <input type="text" name="largePriceName" class="priceHeading" value="Large Size Price" readonly>
-                                    <input type="Number" name="largePrice" class="prices" value="<?php if (isset($largePrice)) {
+                <p>
+                  <input
+                    type="text"
+                    name="largePriceName"
+                    class="priceHeading"
+                    value="Large Size Price"
+                    readonly
+                  />
+                  <input type="Number" name="largePrice" class="prices" value="<?php if (isset($largePrice)) {
                                                                                                         echo $largePrice;
                                                                                                     } ?>">
-                                </p>
-                            </li>
+                </p>
+              </li>
+          
+            
+       
 
-
-                            <li class="description">
-                                <p>
-                                    Enter Description Category<span>*</span>
-                                </p>
-                                <textarea value="description" name="description" required><?php if (isset($description)) {
+            <li class="description">
+              <p>
+                Enter Description Category<span>*</span>
+              </p>
+              <textarea value="description" name="description" required><?php if (isset($description)) {
                                                                                                 echo $description;
                                                                                             } ?></textarea>
-                            </li>
-                            <li>
-                                <p> Upload Coffee Photo <span>*</span>
-                                </p>
-                                <input type="file" name="CoffeeImage" id="CoffeeImage" onchange="loadFile(event)">
-                            </li>
-                            <li>
-                                <p>Photo Realtime Preview</p>
-                                <div class="preview_photo">
+            </li>
+            <li>
+              <p>
+                Upload Food
+                Photo <span>*</span>
+              </p>
+              <input type="file" name="CoffeeImage" id="CoffeeImage" onchange="loadFile(event)">
+            </li>
+            <li>
+              <p>Photo Realtime Preview</p>
+              <div class="preview_photo">
+                
+                  <div class="uploadImage">
 
-                                    <img alt="Coffee images" id="CoffeeImagePreview" layout="fill" />
+                  <img
+                   
+                    alt="item images"
+                    id="FoodImagePreview"
+                  
+                  />
+                  </div>
+                
+              </div>
+            </li>
 
-
-                                </div>
-                            </li>
-
-                            <li class="btns">
+            <li class="btns">
                                 <p>Product Visibility Status </p>
                                 <label class="switch">
 
@@ -170,16 +221,20 @@
 
                                 </label>
                             </li>
-                            <button name="SendCoffeeItem">
-                                ADD COFFEE
-                            </button>
-                        </div>
-                    </form>
-
-
-                </div>
-
+            <button name="SendCoffeeItem">
+              ADD COFFEE
+            </button>
+          </div>
+                                                                                        </form>
         </div>
+      </div>
+
+
+
+
+
+
+     
 
     </div>
 
@@ -187,8 +242,8 @@
         var loadFile = function(event) {
             var reader = new FileReader();
             reader.onload = function() {
-                document.getElementById('CoffeeImagePreview').style.display = "block";
-                var preview = document.getElementById('CoffeeImagePreview');
+                document.getElementById('FoodImagePreview').style.display = "block";
+                var preview = document.getElementById('FoodImagePreview');
                 preview.src = reader.result;
             }
             reader.readAsDataURL(event.target.files[0]);
--- admin/adddrinkitem.php
+++ admin/adddrinkitem.php
@@ -9,7 +9,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php require('../modules/HeadTag.php'); ?>
 
-<link rel="stylesheet" href="../styles/admin/admin.css?v=5">
+<link rel="stylesheet" href="../styles/admin/admin.css?v=18">
 <script>
     window.document.title = "SD CANTEEN | Add Drink Item";
     // prevent reload post request
@@ -27,138 +27,189 @@
         <!-- left side bar import -->
         <?php require('../components/LeftAdminHeader.php'); ?>
 
-        <!-- right top header -->
+
+
         <div class="rightsidebar">
-            <?php $AdminTopHeaderTitle = "Add Drink Page";
+     
+        <?php $AdminTopHeaderTitle = "Add Drink Page";
             require('../components/AdminTopHeader.php'); ?>
 
 
 
             <!-- path navigation -->
             <?php $pathNavigationParentPath = "/sd-canteen/admin/viewdrinkItem.php";
-            $pathNavigationParent = "Drink";
+            $pathNavigationParent = "Drinks";
             $pathNavigationChild = "add drink item";
             require('../components/PathNavigation.php'); ?>
 
-            <div class="FoodPage">
+   
 
-                <div class="Form" style="margin-top:-8%">
-                    <form action="" method="post" enctype="multipart/form-data">
-                        <div class="heading">
-                            <h1>
-                                Enter New DrinkItem Item For Website
-                            </h1>
-                        </div>
-                        <div class="form_element">
-                            <li>
-                                <p>
-                                    Enter Drink Name <span>*</span>
-                                </p>
-                                <input type="text" name="DrinkName" value="<?php if (isset($DrinkName)) {
+        <div class="Form">
+          <div class="heading">
+            <h1>
+              Enter New Drink   Item For Website
+            </h1>
+          </div>
+          <form method="post" action=""  enctype="multipart/form-data">
+          <div class="form_element">
+            <li>
+              <p>
+                Enter
+                Drink
+                Name <span>*</span>
+              </p>
+              <input type="text" name="DrinkName" value="<?php if (isset($DrinkName)) {
                                                                                 echo $DrinkName;
                                                                             } ?>"
                                                                             autofocus required>
-                            </li>
+                
+              
+            </li>
 
-                            <li>
-                                <p>
-                                    Enter Drink Qty
-                                </p>
-                                <input type="Number" name="DrinkQty" value="1" required>
-                            </li>
+            <li>
+              <p>
+                Enter Drink
+                Qty
+              </p>
+              <input
+                type="text"
+                name="DrinkQty"
+               value="1"
+               readonly
+          
+              />
+            </li>
 
-                            <li class="selects">
-                                <p>
-                                    Enter Drink Category <span>*</span>
-                                </p>
-                                <select name="DrinkCategory">
+            <li class="selects">
+              <p>
+                Enter
+                Drink
+                Category <span>*</span>
+              </p>
+              <select
+                name="DrinkCategory"
+             
+                
+              >
+                <option value="no">Select Category</option>
+                
+                <?php
 
+require('../middleware/ConnectToDatabase.php');
+$sql_query = "select * from drinkcategories";
+$resFoodItem = mysqli_query($connection, $sql_query);
 
-                                    <option value="no">Select drink category</option>
+$length = mysqli_num_rows($resFoodItem);
 
+if ($length == 0) {
+    echo "<h6>No Item Found</h6>";
+} else {
+    while ($dataSearch = mysqli_fetch_array($resFoodItem)) {
+        echo "<option>";
+        echo $dataSearch['drinkcategoryname'];
+        echo "</option>";
+    }
+}
+?>
+                     
+              </select>
+            </li>
 
-                                    <!-- load category name -->
-                                    <?php
+       
 
-                                    require('../middleware/ConnectToDatabase.php');
-                                    $sql_query = "select * from drinkcategories";
-                                    $resFoodItem = mysqli_query($connection, $sql_query);
-
-                                    $length = mysqli_num_rows($resFoodItem);
-
-                                    if ($length == 0) {
-                                        echo "<h6>No Item Found</h6>";
-                                    } else {
-                                        while ($dataSearch = mysqli_fetch_array($resFoodItem)) {
-                                            echo "<option>";
-                                            echo $dataSearch['drinkcategoryname'];
-                                            echo "</option>";
-                                        }
-                                    }
-                                    ?>
-
-                                </select>
-                            </li>
-
-
-                            <li class="Prices">
-                                <h6>
-                                    Enter Price <span>*</span>
-                                </h6>
-                                <p>
-                                    <input type="text" name="normalPriceName" class="priceHeading" value="Normal Size Price" readonly>
-                                    <input type="Number" name="normalPrice" class="prices" value="<?php if (isset($normalPrice)) {
+              <li class="Pricess">
+                <h6>
+                  Enter Price <span>*</span>
+                </h6>
+                <p>
+                  <input
+                    type="text"
+                    name="normalPriceName"
+                    class="priceHeading"
+                    value="Normal Size Price" readonly
+                  >
+                  <input type="Number" name="normalPrice" class="prices" value="<?php if (isset($normalPrice)) {
                                                                                                         echo $normalPrice;
                                                                                                     } ?>">
-                                </p>
-                                <h4>Or</h4>
-                                <p>
-                                    <input type="text" name="smallPriceName" class="priceHeading" value="Small Size Price" readonly>
-                                    <input type="Number" name="smallPrice" class="prices" value="<?php if (isset($smallPrice)) {
+                </p>
+                <h4>Or</h4>
+                <p>
+                  <input
+                    type="text"
+                    name="smallPriceName"
+                    class="priceHeading"
+                    value="Small Size Price" 
+                    readonly
+                  >
+                  <input type="Number" name="smallPrice" class="prices" value="<?php if (isset($smallPrice)) {
                                                                                                         echo $smallPrice;
                                                                                                     } ?>">
-                                </p>
+                </p>
 
-                                <p>
-                                    <input type="text" name="mediumPriceName" class="priceHeading" value="Medium Size Price" readonly>
-                                    <input type="Number" name="mediumPrice" class="prices" value="<?php if (isset($mediumPrice)) {
+                <p>
+                  <input
+                    type="text"
+                    name="mediumPriceName"
+                    class="priceHeading"
+                
+                    value="Medium Size Price"
+                    readonly
+                  />
+                  <input type="Number" name="mediumPrice" class="prices" value="<?php if (isset($mediumPrice)) {
                                                                                                         echo $mediumPrice;
                                                                                                     } ?>">
-                                </p>
+                </p>
 
-                                <p>
-                                    <input type="text" name="largePriceName" class="priceHeading" value="Large Size Price" readonly>
-                                    <input type="Number" name="largePrice" class="prices" value="<?php if (isset($largePrice)) {
+                <p>
+                  <input
+                    type="text"
+                    name="largePriceName"
+                    class="priceHeading"
+                    value="Large Size Price"
+                    readonly
+                  />
+                  <input type="Number" name="largePrice" class="prices" value="<?php if (isset($largePrice)) {
                                                                                                         echo $largePrice;
                                                                                                     } ?>">
-                                </p>
-                            </li>
+                </p>
+              </li>
+          
+            
+       
 
-
-                            <li class="description">
-                                <p>
-                                    Enter Description Category<span>*</span>
-                                </p>
-                                <textarea value="description" name="description" required><?php if (isset($description)) {
+            <li class="description">
+              <p>
+                Enter Description Category<span>*</span>
+              </p>
+              <textarea value="description" name="description" required><?php if (isset($description)) {
                                                                                                 echo $description;
                                                                                             } ?></textarea>
-                            </li>
-                            <li>
-                                <p> Upload Drink Photo <span>*</span>
-                                </p>
-                                <input type="file" name="DrinkImage" id="CoffeeImage" onchange="loadFile(event)">
-                            </li>
-                            <li>
-                                <p>Photo Realtime Preview</p>
-                                <div class="preview_photo">
+            </li>
+            <li>
+              <p>
+                Upload Food
+                Photo <span>*</span>
+              </p>
+              <input type="file" name="DrinkImage" id="CoffeeImage" onchange="loadFile(event)">
+            </li>
+            <li>
+              <p>Photo Realtime Preview</p>
+              <div class="preview_photo">
+                
+                  <div class="uploadImage">
 
-                                    <img alt="Drink images" id="CoffeeImagePreview" layout="fill" />
+                  <img
+                   
+                    alt="item images"
+                    id="FoodImagePreview"
+                  
+                  />
+                  </div>
+                
+              </div>
+            </li>
 
-
-                                </div>
-                            </li>
-
-                            <li class="btns">
+            <li class="btns">
                                 <p>Product Visibility Status </p>
                                 <label class="switch">
 
@@ -170,17 +221,20 @@
 
                                 </label>
                             </li>
-                            <button name="SendDrinkItem">
-                                ADD Drink
-                            </button>
-                        </div>
-                    </form>
-
-
-                </div>
-
-            </div>
+            <button name="SendDrinkItem">
+              ADD DRINK
+            </button>
+          </div>
+                                                                                        </form>
         </div>
+      </div>
+
+
+
+
+
+
+     
 
     </div>
 
@@ -188,8 +242,8 @@
         var loadFile = function(event) {
             var reader = new FileReader();
             reader.onload = function() {
-                document.getElementById('CoffeeImagePreview').style.display = "block";
-                var preview = document.getElementById('CoffeeImagePreview');
+                document.getElementById('FoodImagePreview').style.display = "block";
+                var preview = document.getElementById('FoodImagePreview');
                 preview.src = reader.result;
             }
             reader.readAsDataURL(event.target.files[0]);
