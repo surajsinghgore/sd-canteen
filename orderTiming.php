fb7a6450550951d57a136e6fe063a3341878c182
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-02-26 10:33:49 +0530

    order timing page created

---
+++ orderTiming.php
@@ -0,0 +1,587 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<?php require('./modules/clientHeadTag.php'); ?>
+
+
+
+<link rel="stylesheet" href="./styles/client/orderdetails.css?v=4">
+<link rel="stylesheet" href="./styles/client/cart.css?v=3">
+
+<link rel="stylesheet" href="./styles/admin/admin.css">
+<script>
+    // prevent reload post request
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, window.location.href)
+    }
+    window.document.title = "SD CANTEEN | Order Timing";
+</script>
+
+<body>
+ 
+
+
+
+
+
+        <!-- header -->
+        <?php require('./components/Header.php'); ?>
+        <div class="admin">
+
+
+
+
+
+
+
+    </div>
+
+   <!-- order timing page -->
+
+ 
+  
+   <div class="cart">
+        <h1> Order Details </h1>
+        <div class="progressTop">
+          <hr />
+
+          <div class="number">
+            <div class="num1">
+              <div class="circle">
+                <span>1</span>
+              </div>
+              <div class="description">
+                <p> SHOPPING CART </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="number">
+            <div class="num1">
+              <div class="circle circle1">
+            
+                <span>2</span>
+              </div>
+              <div class="description">
+                <p> ORDER DETAILS </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="number">
+            <div class="num1">
+              <div class="circle">
+             
+                <span>3</span>
+              </div>
+              <div class="description">
+                <p> PAYMENT METHOD </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="orderDetails">
+          <div class="orderTable">
+          
+              <div>
+                <h4>Please order food 10 minutes before pickup</h4>
+                <h3>Select Pickup Time</h3>
+              </div>
+            
+          </div>
+          <div class="TimeBox">
+          
+       
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+         
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+
+
+              <div class="box" >
+                <div class="btn">
+                
+                    <input
+                      type="radio"
+                      name="time"
+                      id=""
+                   
+                    />
+                
+                 
+            
+                </div>
+
+                <div class="time">
+                  <h4>
+                    <label for="time">
+                      12:30 PM
+                    </label>
+                  </h4>
+                </div>
+              </div>
+          </div>
+
+          <div class="BottomMessage">
+           
+              <!-- <div class="message">
+                Order is Not allowed to Placed after <span>5.51 PM</span> from
+                <span>Monday</span> To <span>Saturday.</span> Closed On
+                <span>Sunday</span> and on <span>Holidays</span>
+              </div> -->
+           
+          
+              <h6>Note: Order Can&#39;t Cancelled Once Placed.</h6>
+            
+          </div>
+
+  
+          <div class="bottom">
+            <Link href="/Cart">
+              <button class="more">Cart Page</button>
+            </Link>
+            <div class="subtotal">
+    
+                <button>Continue Order</button>
+             
+                <!-- <button
+                  style={{ cursor: "not-allowed" }}
+                  onClick={() => getTime()}
+                  disabled
+                >
+                  Continue Order
+                </button> -->
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+    
+ 
+
+    <!-- footer -->
+    <?php require('./components/Footer.php'); ?>
+
+   
+</body>
+
+</html>
