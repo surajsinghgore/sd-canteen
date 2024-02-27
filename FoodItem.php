e1b01331fd1ecedddc489472c6bd2518671d65a3
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-02-27 14:08:24 +0530

    food item search bar working

--- FoodItem.php
+++ FoodItem.php
@@ -85,7 +85,7 @@
 
 <h4>Showing all 15  results</h4>
   <div class="search">
-<input type="search" name="search" id="search1" placeholder="Search Item ..."/>
+<input type="search" name="search" id="itemSearchBar" onkeyup="searchByItemName()" placeholder="Search Item ..."/>
 <div class="btn">
 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg>
 </div>
@@ -177,6 +177,35 @@
 
 
       <script>
+
+        // search by item name
+        const searchByItemName=()=>{
+         
+let searchInput=document.getElementById('itemSearchBar').value;
+$.ajax({
+                    type: "POST", //type of method
+                    url: "http://localhost/sd-canteen/clientApi/searchFoodItems.php", //your page
+                    data: {
+                        itemName: searchInput
+                    }, // passing the values
+                    success: function(res) {
+  
+                        document.getElementById('loadItems').innerHTML=res;
+                        let cartData1=localStorage.getItem('cartItem');
+let cartDataConvert1=JSON.parse(cartData1);
+for(let i=0;i<cartDataConvert1.items.length;i++){
+    document.getElementById(`removeToCartBtn${cartDataConvert1.items[i].id}`).style.display="block";
+    document.getElementById(`addToCartBtn${cartDataConvert1.items[i].id}`).style.display="none";
+    
+    document.getElementById(`currentSizeSelect${cartDataConvert1.items[i].id}`).innerText=cartDataConvert1.items[i].size;
+    
+    document.getElementById(`price${cartDataConvert1.items[i].id}`).innerHTML=`<h4>₹${cartDataConvert1.items[i].price}</h4>`;
+}
+                   
+
+                    }
+                });
+        }
     //  loading left side category
 const pageLoadData=()=>{
 // loading category data
@@ -214,23 +243,7 @@ $.ajax({
 
 
 
-            // search with name
-            function searchByName() {
-                let searchInput = document.getElementById('foodNameSearch').value;
-
-                $.ajax({
-                    type: "POST", //type of method
-                    url: "http://localhost/sd-canteen/api/searchDrinkItemByName.php", //your page
-                    data: {
-                        foodname: searchInput
-                    }, // passing the values
-                    success: function(res) {
-                        document.getElementById('resultData').innerHTML = res;
-                    }
-                });
-
-            }
-
+         
 
 
 
@@ -431,6 +444,15 @@ let convertCartDataToJson=JSON.parse(cartData);
 
 // convert res data to json
 let itemData=JSON.parse(res);
+
+// check weather item is already present in cart or not
+for (let i = 0; i < convertCartDataToJson.items.length; i++) {
+if(convertCartDataToJson.items[i].itemName==itemData.itemName && convertCartDataToJson.items[i].id==itemData.id){
+  
+    document.location.href="http://localhost/sd-canteen/cart.php";
+return;
+}
+}
 // calculate cartTotal cost
 let total=0;
 
