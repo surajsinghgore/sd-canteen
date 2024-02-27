d8ddc90e3b3f3f2156b0c34bb2a0d990e5f708c6
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-02-27 20:40:11 +0530

    gallery page working

--- gallery.php
+++ gallery.php
@@ -117,7 +117,10 @@ x
 <!-- Initialize Swiper -->
 <script>
 
-
+const OpenInLargeView=(item)=>{
+    document.getElementById('full').style.display="block";
+    
+}
 const closeMenu=()=>{
 
     document.getElementById('full').style.display="none";
@@ -148,7 +151,7 @@ const closeMenu=()=>{
 // GalleryData
 
 import data from './data/Gallery.js';
-data.map((item)=>{
+data.map((item,index)=>{
 document.getElementById('swiperItemAdd').innerHTML+=`<div class="swiper-slide">
       <div class="inner">
            <div class="imageSection">
@@ -165,7 +168,7 @@ document.getElementById('swiperItemAdd').innerHTML+=`<div class="swiper-slide">
     document.getElementById('GalleryData').innerHTML+=`
     <div class="card" >
  <div class="fulls">
- <div class="cont" title="Full Screen">
+ <div class="cont" title="Full Screen" onclick='OpenInLargeView("${index}")'>
  <svg stroke="currentColor" class="iconsFull" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" class="Gallery_iconsFull___KmPX" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707zm0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707zm-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707z" ></path></svg>
 </div>
  
