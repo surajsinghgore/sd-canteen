77ecaf9096b367bf19d24e21a8902020e1d50c27
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-03-01 15:44:33 +0530

    email send about order placed in cod mode

--- pgRedirect.php
+++ pgRedirect.php
@@ -88,16 +88,7 @@ if($rowCount==0){
 	$paramList["VERIFIED_BY"] = $email;
 	$paramList["MSISDN"] = $mobile;
 	$paramList["IS_USER_VERIFIED"] = "YES";
-	/*
-	$paramList["CALLBACK_URL"] = "http://localhost/PaytmKit/pgResponse.php";
-	$paramList["MSISDN"] = $MSISDN; //Mobile number of customer
-	$paramList["EMAIL"] = $EMAIL; //Email ID of customer
-	// $paramList["VERIFIED_BY"] = "EMAIL"; //
-	$paramList["IS_USER_VERIFIED"] = "YES"; //
 	
-	*/
-	
-	//Here checksum string will return by getChecksumFromArray() function.
 	$checkSum = getChecksumFromArray($paramList,PAYTM_MERCHANT_KEY);
 	
 	?>
@@ -195,10 +186,132 @@ for ($i=0; $i <$countLength ; $i++) {
 $DataGetsRes = mysqli_query($connection, $insertItemsQuery);
 
 }
+// arrange cart data
 
 
 
-header("Location: /sd-canteen/ordercomplete.php");
+$email_body="<html>
+<head>
+<title>Order Placed </title>
+</head>
+
+<body>
+<div>
+<img src='https://res.cloudinary.com/dnxv21hr0/image/upload/v1681014809/ClientImages/logo_l0f3ug.png' alt='logo' height='50px' width='130px' style='margin-left:30vw'>
+</div>
+<div style='color:rgb(245, 245, 245);text-align: center;width: 99%;font-size:3vw;background-color: rgb(245, 61, 0);padding-top: 1%;padding-bottom: 1%;'>SD CANTEEN</div>
+    <h1 style='margin-top:4%;font-size:3vw;color:rgb(0, 0, 0)'>Hi,<span style='color:rgb(231, 5, 5);'>$fullname</span> </h1>
+    <div style='color:black;text-align: center;width: 100%;font-size:3vw;'>Thank You For Your Order</div>
+      <div style='margin-top:4%;color:rgb(95, 95, 95);text-align: center;width: 100%;font-size:2vw;'>Your 6 Digit Token Number:</div>
+      <div style='margin-top:4%;color:rgb(198, 10, 10);text-align: center;width: 99%;font-size:3vw;border: 3px dotted rgb(255, 38, 0);text-transform: lowercase;'>$orderId</div>
+        <div style='margin-top:6%;color:rgb(95, 95, 95);width: 100%;font-size:2vw;'>Your Order Items Details:</div>
+    <table style='margin-top:2%;width:100%;text-align: center;font-size: 2vw;border:2px solid black'>
+    <tr style='background-color: rgb(238, 238, 238);color:black'>
+    <th>Item Name</th>
+    <th>Price</th>
+    <th>Size</th>
+    <th>Qty</th>
+    <th>Category</th>
+    <th>Total</th>
+    </tr>";
+
+
+
+
+
+	for ($i=0; $i <$countLength ; $i++) { 
+		$cartItemName=$item[$i]['itemName'];
+		$cartItemPrice=$item[$i]['price'];
+		$cartItemQtyBook=$item[$i]['qtyBook'];
+		$cartItemSize=$item[$i]['size'];
+		$cartItemMainCategory=$item[$i]['itemMainCategory'];
+		$cartItemCategory=$item[$i]['category'];
+		$totalAmount=$cartItemQtyBook*$cartItemPrice;
+		$email_body .="
+	<tr style='border: 1px solid rgb(220, 220, 220);color:rgb(255, 81, 0);text-transform: capitalize;'>
+	<td>$cartItemName</td>
+	<td>$cartItemPrice</td>
+	<td>$cartItemSize</td>
+	<td>$cartItemQtyBook</td>
+	<td>$cartItemCategory</td>
+	<td>$totalAmount</td>
+	</tr>
+	";
+	}
+
+
+
+	$email_body .="</table>
+
+	<div style='margin-Top:4%;width:100%;display:flex;font-size: 2vw;'>
+	<div style='color:rgb(28, 28, 28)'>Total Items Order : </div>
+	<div style='padding-left:3%;color:rgb(66, 66, 66)'>$countLength</div>
+	 </div>
+	<div style='margin-Top:4%;width:100%;display:flex;border-top: 4px solid rgb(213, 213, 213);border-bottom: 4px solid rgb(213, 213, 213);font-size: 3vw;text-align: center;color: rgb(49, 49, 49);'>
+	<div style='width:50%'>Total Amount</div>
+	<div style='width:50%'>₹ $TXN_AMOUNT</div>
+	 </div>
+	<div style='margin-Top:4%;width:100%;display:flex;font-size: 2vw;'>
+	<div style='color:rgb(28, 28, 28)'>PickUp Time : </div>
+	<div style='padding-left:3%;color:rgb(2, 151, 205)'>$pickUpTime</div>
+	 </div>
+	 <div style='margin-Top:2%;width:100%;display:flex;font-size: 2vw;'>
+	<div style='color:rgb(28, 28, 28)'>Order Time : </div>
+	<div style='padding-left:3%;color:rgb(2, 151, 205)'>$currentTime</div>
+	 </div>
+	 <div style='margin-Top:2%;width:100%;display:flex;font-size: 2vw;'>
+	<div style='color:rgb(28, 28, 28)'>Order Date : </div>
+	<div style='padding-left:3%;color:rgb(2, 151, 205)'>$currentDate</div>
+	 </div>
+	 <div style='margin-Top:2%;width:100%;display:flex;font-size: 2vw;'>
+	<div style='color:rgb(28, 28, 28)'>Payment Method : </div>
+	<div style='padding-left:3%;color:rgb(2, 151, 205)'>$payment</div>
+	 </div>
+	 <div style='margin-Top:2%;width:100%;display:flex;font-size: 2vw;'>
+	<div style='color:rgb(28, 28, 28)'>Amount Paid : </div>
+	<div style='padding-left:3%;color:rgb(2, 151, 205)'>0</div>
+	 </div>
+	  <div style='margin-Top:5%;width:100%;font-size: 2vw;text-align: center;'>
+	<div style='color:rgb(117, 117, 117)'>Please contact SD CANTEEN if you have not placed an order or if the order was placed by mistake.</div>
+	 </div>
+	  <div style='margin-top:4%;color:rgb(245, 245, 245);text-align: center;width: 99%;font-size:3vw;background-color: rgb(245, 61, 0);padding-top: 1%;padding-bottom: 1%;'>SD CANTEEN</div>
+	
+	
+	</body>
+	</html>";
+// send email about order placed
+
+
+
+$to = $email;
+$subject = "Order Placed ";
+$message = "";
+
+
+    
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\b";
+$headers .= 'From: prpbrainbooster@gmail.com' . "\r\n";
+
+
+$result = mail($to, $subject, $email_body, $headers);
+if ($result == true) {
+	header("Location: /sd-canteen/ordercomplete.php");
+
+}else{
+
+	echo'ss';
+}
+
+
+
+
+
+
+
+
 
 }
 
